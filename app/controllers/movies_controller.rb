74d013ab3f77d1457a9ddf899f2c4fb4b9846137
zfk2107 Zaryaab Khan <zfk2107@columbia.edu>
2025-09-30 21:44:53 +0000

    feature added for filtering movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,11 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings     = Movie.all_ratings
+    @ratings_to_show = (params[:ratings]&.keys || @all_ratings)
+    @movies          = Movie.with_ratings(@ratings_to_show)
   end
+  
 
   def new
     # default: render 'new' template
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+  RATINGS = %w[G PG PG-13 R].freeze
+
+  def self.all_ratings
+    RATINGS
+  end
+
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil?
+      all
+    else
+      where(rating: ratings_list)
+    end
+  end
 end
